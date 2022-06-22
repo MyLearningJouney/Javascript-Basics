c46e3b3ddfbf8e19c20b4d0a830731b3041a6fd9
Danilo Packer <danilo.kpacker@gmail.com>
2022-06-22 18:57:40 +0000

    commit

--- activities-schedule/js/complete-activity.js
+++ activities-schedule/js/complete-activity.js
@@ -2,9 +2,11 @@ var list = document.querySelector("#activities-scheduled");
 
 list.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(event.target)
     if(event.target.className == 'btn-complete'){
         var desc = document.querySelector(".active-activity");
-        
+        var nodes = Array.from( list.children );
+        var index = nodes.indexOf(desc);
+        console.log(index);
+
     }
 });
