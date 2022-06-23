bc9487cdb5a4253394a35232b6ffa616eece94aa
Danilo Packer <danilo.kpacker@gmail.com>
2022-06-23 13:13:29 +0000

    commit

--- activities-schedule/js/edit-activity.js
+++ activities-schedule/js/edit-activity.js
@@ -8,30 +8,51 @@ list.addEventListener("click", function(event) {
 
     if(event.target.className == 'btn-delete'){
         ul.removeChild(li);
+        return;
     }
+
     if(event.target.className == 'btn-complete'){
-        button.classList.add("invisible")
+        button.classList.remove('btn-complete')
+        button.classList.add("btn-uncheck")
+        button.textContent = "Uncheck"
         li.classList.add("completed")
-        
+        li.querySelector('.btn-edit').remove()
+        return;
     }
+
+    if(event.target.className == 'btn-uncheck'){
+        li.querySelector('.btn-uncheck').remove()
+        li.querySelector('.btn-delete').remove()
+        li.classList.remove("completed")
+        CreateButtons(li);
+        return;
+    }
+
     if(event.target.className == 'btn-edit'){
         button.classList.remove('btn-edit')
         button.classList.add('btn-edit-confirm')
         button.textContent = "Confirm"
+        var input = document.createElement("input");
+        input.classList.add("input-edit")
+        li.append(input);
+        return;
     }
+
     if(event.target.className == 'btn-edit-confirm'){
-        console.log(event.target)
         button.classList.remove('btn-edit-confirm')
         button.classList.add('btn-edit')
-        button.textContent = "AAA"
-        console.log(button)
+        button.textContent = "Edit"
+        var text = li.querySelector('.input-edit').value
+        li.textContent = text
+        CreateButtons(li);
+        console.log(text)
+        return;
     }
 });
 
-
-function getEditActivity(input){
-    editText = {
-        editDesc : input.value
-    }
-    return editText
+function CreateButtons(li){
+    li.appendChild(createCompleteBtn());
+    li.appendChild(createEditBtn());
+    li.appendChild(createDelBtn());
 }
+
