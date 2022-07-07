c339747bee24109ba091467bc65fe1651d383163
Danilo Packer <danilo.kpacker@gmail.com>
2022-07-07 17:54:03 +0000

    commit

--- todo-list-MVC/js/app/views/ActivityView.js
+++ todo-list-MVC/js/app/views/ActivityView.js
@@ -14,10 +14,8 @@ class ActivityView{
         li.classList.add('listItem')
         li.classList.add(`${arrayOfActivities.slice(-1).map((n) => n.id)}`)
         console.log(arrayOfActivities)
-        li.innerHTML = this.template(arrayOfActivities)
+        li.innerHTML = this.template(arrayOfActivities.slice(-1))
         this.#list.append(li)
-        //console.log(this.#list)//html
-        //console.log(ActivityList)//objeto
     }
 
     editingListItem(li,activityObject,span){
@@ -26,21 +24,7 @@ class ActivityView{
         this.setEditInputAttributes(activityObject.activity,input)
         li.innerHTML = this.template([activityObject])
         li.children[0].prepend(input)
-        document.querySelector('form').classList.add('hide')
     }
-
-    hideButtons(activityObject,arrayOfsiblings){
-        //console.log(arrayOfsiblings)
-        arrayOfsiblings.map(liElement => liElement.innerHTML = this.template([activityObject]))
-        //console.log(arrayOfsiblings[0].getElementsByClassName("listBtnWrapper"))
-        //activityObject.icons.map((icon) => Object.values(icon)[0].classList.add("hide"))
-        //console.log(Object.values(activityObject.icons[0])[0])
-    }
-
-    showButtons(activityObject,arrayOfsiblings){
-        arrayOfsiblings.map(liElement => liElement.innerHTML = this.template([activityObject]))
-    }
-
 
     setEditInputAttributes(str,element){
         const attributes = {
